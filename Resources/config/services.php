c276ee6ce9e9319709c86927446c4a4a855fd0cf
Yoann Gauchard <yoann.gauchard@gmail.com>
2024-12-18 15:52:10 +0100

    Fix service declaration using FileLocator to find mapping xml files automatically

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -3,18 +3,21 @@
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Persistence\Mapping\Driver\SymfonyFileLocator;
 
 /** @var \Symfony\Component\DependencyInjection\ContainerBuilder $container */
-$container->setDefinition('evence.softdeletale.listener.softdelete', new Definition('Evence\Bundle\SoftDeleteableExtensionBundle\EventListener\SoftDeleteListener', array(new Reference('annotation_reader', ContainerInterface::NULL_ON_INVALID_REFERENCE))))
+$container->setDefinition('evence.doctrine.orm.xml_driver', new Definition(
+    'Evence\Bundle\SoftDeleteableExtensionBundle\Mapping\Driver\XmlDriver'
+))
+    ->setArguments([
+        new Reference('doctrine.orm.file_locator')
+    ])
+    ->addTag('doctrine.driver');
+
+$container->setDefinition('evence.softdeletale.listener.softdelete', new Definition(
+    'Evence\Bundle\SoftDeleteableExtensionBundle\EventListener\SoftDeleteListener',
+    [new Reference('annotation_reader', ContainerInterface::NULL_ON_INVALID_REFERENCE)]
+))
     ->addTag('doctrine.event_listener', array(
         'event' => 'preSoftDelete',
     ));
-
-$container->setDefinition('evence.doctrine.orm.xml_driver', new Definition(
-    'Evence\Bundle\SoftDeleteableExtensionBundle\Mapping\Driver\Xml',
-    [
-        ['%kernel.project_dir%/config/doctrine'],
-        '.orm.xml'
-    ]
-))
-    ->addTag('doctrine.driver');
